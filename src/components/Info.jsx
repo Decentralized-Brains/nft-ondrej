bbd7aa4cc5f719efc0dc67e1ec87fc8846cc7551
Rashid Shahriar <rashidslink@gmail.com>
2023-02-18 22:48:08 +0600

    collector fond added

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { DumyCard } from "./Data";
+import InfoTableOne from "./InfoTableOne";
+import InfoTableTwo from "./InfoTableTwo";
 import { useLocation } from "react-router-dom";
 
 const Info = () => {
@@ -10,128 +10,37 @@ const Info = () => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  const navigate = useNavigate();
-  const handleShowCard = (id) => {
-    navigate(`/about/${id}`);
-  };
   return (
-    <div className="container  my-16 md:my-[50px] lg:my-[150px]">
-      <div className="md:flex md:justify-center md:gap-[70px]">
-        {/* This is card */}
-        <div className="hidden lg:grid  md:gap-[81px] ">
-          {DumyCard.map((record) => {
-            return (
-              <div
-                onClick={() => {
-                  handleShowCard(record.id);
-                }}
-                className="w-[195px] h-[250px] bg-[#DEAF07] p-[6px] rounded cursor-pointer"
-                key={record.id}
-              >
-                <img
-                  src={record.img}
-                  alt=""
-                  className=" w-[181px] h-[181px] mx-auto rounded"
-                />
-                <h2 className="text-[14px] font-bold py-[5px]">
-                  {record.title}
-                </h2>
-                <p className=" text-[8px]">{record.details}</p>
+    <div className="container  my-16 md:my-[60px] ">
+      <div>
+        {/* Button */}
+        <div>
+          <div className="flex justify-between items-end">
+            <button className="text-base px-4 md:px-[40px] py-2 md:py-[10px] bg-[#FCC607]">
+              Enter To Competition
+            </button>
+            <div>
+              <div className="text-[#D9D9D9] text-base pb-2">
+                THIS MONTH ENDS IN
               </div>
-            );
-          })}
-        </div>
-
-        {/* This is table */}
-
-        <div className="border border-[#DEAF07] rounded">
-          <table className="mx-auto w-full md:w-[500px] xl:w-[690px] divide-y divide-[#DEAF07] ">
-            <thead>
-              <tr>
-                <th
-                  scope="col"
-                  className="px-5 md:px-6 py-3 text-left font-medium text-[#FCC607] uppercase"
-                >
-                  Name
-                </th>
-                <th
-                  scope="col"
-                  className="px-5 md:px-6 py-3 text-left font-medium text-[#FCC607] uppercase"
-                >
-                  Age
-                </th>
-                <th
-                  scope="col"
-                  className="px-5 md:px-6 py-3 text-left font-medium text-[#FCC607] uppercase"
-                >
-                  Address
-                </th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-              <tr>
-                <td className="px-5 md:px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
-                  John Brown
-                </td>
-                <td className="px-5 md:px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                  45
-                </td>
-                <td className="px-5 md:px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                  New York No.
-                </td>
-              </tr>
-
-              <tr>
-                <td className="px-5 md:px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
-                  Jim Green
-                </td>
-                <td className="px-5 md:px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                  27
-                </td>
-                <td className="px-5 md:px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                  London No.
-                </td>
-              </tr>
-
-              <tr>
-                <td className="px-5 md:px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
-                  Joe Black
-                </td>
-                <td className="px-5 md:px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                  31
-                </td>
-                <td className="px-5 md:px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                  Sidney No.
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        {/* This is card */}
-        <div className="hidden lg:grid  gap-y-[81px]">
-          {DumyCard.map((record) => {
-            return (
-              <div
-                onClick={() => {
-                  handleShowCard(record.id);
-                }}
-                className="w-[195px] h-[250px] bg-[#DEAF07] p-[6px] rounded cursor-pointer"
-                key={record.id}
-              >
-                <img
-                  src={record.img}
-                  alt=""
-                  className=" w-[181px] h-[181px] mx-auto rounded"
-                />
-                <h2 className="text-[14px] font-bold py-[5px]">
-                  {record.title}
-                </h2>
-                <p className=" text-[8px]">{record.details}</p>
+              <div className="text-base flex gap-1 md:gap-2 items-center">
+                <span className="bg-[#FCC607] p-1 md:p-[10px] rounded">01</span>
+                <span className="text-[#D9D9D9]">:</span>
+                <span className="bg-[#FCC607] p-1 md:p-[10px] rounded">17</span>
+                <span className="text-[#D9D9D9]">:</span>
+                <span className="bg-[#FCC607] p-1 md:p-[10px] rounded">42</span>
+                <span className="text-[#D9D9D9]">:</span>
+                <span className="bg-[#FCC607] p-1 md:p-[10px] rounded">25</span>
               </div>
-            );
-          })}
+            </div>
+          </div>
         </div>
+
+        {/* Table 1*/}
+        <InfoTableOne />
+
+        {/* Table 2*/}
+        <InfoTableTwo />
       </div>
     </div>
   );
---
+++ src/components/InfoTableTwo.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import ProfilePic from "../assets/propic.jpg";
+
+const InfoTableTwo = () => {
+  return (
+    <div className="my-16">
+      <button className="text-base px-4 md:px-[40px] py-2 md:py-[10px] bg-[#FCC607] rounded mb-10">
+        TOP 11-20
+      </button>
+      <div className=" overflow-x-auto sm:rounded-lg border border-[#FCC607]">
+        <table className="w-full text-sm md:text-base  text-[#D9D9D9] text-center">
+          <thead className="text-xs md:text-base uppercase text-[#FCC607] border-b border-gray-600">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Username
+              </th>
+              <th scope="col" className="px-4 py-3">
+                NFT(Bought)
+              </th>
+              <th scope="col" className="px-4 py-3">
+                Spent
+              </th>
+              <th scope="col" className="px-4 py-3">
+                Wallet Address
+              </th>
+              <th scope="col" className="px-4 py-3">
+                Top 10(This Month)
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="hover:bg-yellow-100 hover:bg-opacity-10">
+              <td className="px-6 py-3 font-medium  ">
+                <div className="flex items-center gap-4">
+                  <div className="w-10 h-10">
+                    <img
+                      src={ProfilePic}
+                      alt="user"
+                      className="w-10 h-10 rounded-full object-cover"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <span>Kelvin Phillips</span>
+                    <span className="text-gray-400">@kelvin_phillp12</span>
+                  </div>
+                </div>
+              </td>
+              <td className="px-4 py-3">6</td>
+              <td className="px-4 py-3">0.0025 ETH</td>
+              <td className="px-4 py-3">#asdjb239594549</td>
+              <td className="px-4 py-3">8</td>
+            </tr>
+            <tr className="hover:bg-yellow-100 hover:bg-opacity-10">
+              <td className="px-6 py-3 font-medium  ">
+                <div className="flex items-center gap-4">
+                  <div className="w-10 h-10">
+                    <img
+                      src={ProfilePic}
+                      alt="user"
+                      className="w-10 h-10 rounded-full object-cover"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <span>Kelvin Phillips</span>
+                    <span className="text-gray-400">@kelvin_phillp12</span>
+                  </div>
+                </div>
+              </td>
+              <td className="px-4 py-3">6</td>
+              <td className="px-4 py-3">0.0025 ETH</td>
+              <td className="px-4 py-3">#asdjb239594549</td>
+              <td className="px-4 py-3">8</td>
+            </tr>
+            <tr className="hover:bg-yellow-100 hover:bg-opacity-10">
+              <td className="px-6 py-3 font-medium  ">
+                <div className="flex items-center gap-4">
+                  <div className="w-10 h-10">
+                    <img
+                      src={ProfilePic}
+                      alt="user"
+                      className="w-10 h-10 rounded-full object-cover"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <span>Kelvin Phillips</span>
+                    <span className="text-gray-400">@kelvin_phillp12</span>
+                  </div>
+                </div>
+              </td>
+              <td className="px-4 py-3">6</td>
+              <td className="px-4 py-3">0.0025 ETH</td>
+              <td className="px-4 py-3">#asdjb239594549</td>
+              <td className="px-4 py-3">8</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default InfoTableTwo;
